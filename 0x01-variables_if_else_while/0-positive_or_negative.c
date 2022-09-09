a2966a6a83bf93fdd0b791e368ecb090f10033fd
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-09 08:32:09 -0700

    Retha

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
-
+#include <stdio.h>
 /**
- * more header files goes on top
- *
  * main - function entry point
+ *
  * Return: returns 0 (zero)
  */
 /*betty style doc for function main goes there */
