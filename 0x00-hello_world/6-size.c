ebc6c98a4aad791d56a180bef274b83aab5c76dd
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-08 19:13:16 -0700

    ab

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- main entry function
+ *
+ * Return: returns 0 (zero)
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
+	printf"Size of a float", sizeof(float);
+
+
+	return (0);
+}
