4c78c84a85e5b02aee54c1c503c978ba715f6482
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-08 18:13:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
 */
 int main(void)
 {
-	char sent[] = "\"Programming is like building a multilingual puzzle"
+	char sent[] = "\"Programming is like building a multilingual puzzle";
 	puts(sent);
 	return (0);
 }
