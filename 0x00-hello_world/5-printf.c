14a34ac86bf16c61b06662b76f330e3b726c9d14
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-08 18:48:27 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,13 @@
 /**
  * main- main entry function
  *
+ *
  * return: returns 0(zero)
  */
 int main(void)
 {
-	char sent[]="with proper grammar, but the outcome is a piece of art,";
+	char sent[] =intf "with proper grammar, but the outcome is a piece of art,\n";
 
-	printf("%s",sent);
+	printf ("%s", sent);
 	return(0);
 }
