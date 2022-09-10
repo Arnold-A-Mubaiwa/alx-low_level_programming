6b45c12209d7a6ab6ce54b54943f0ef1b9729d4e
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-10 03:48:23 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - main is the entry point of a program
+ *
+ * Return: returns 0 (zero)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x;
+	int y = 0;
+
+	for(x = 0; x < 2; x++)
+	{
+		while(y <= 25)
+		{
+			if (x == 0)
+				putchar(alphabet[y]);
+			else
+				putchar(toupper(alphabet[y]));
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
