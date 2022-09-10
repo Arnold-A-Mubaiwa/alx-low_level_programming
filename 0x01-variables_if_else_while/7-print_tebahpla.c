e4da1b0518c203e398c1fb60afedbfd46f41a97a
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-10 06:54:01 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of a program
+ *
+ * Return: return 0 (zero)
+ */
+int main(void)
+{
+	int x = 25;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (x >= 0)
+	{
+		putchar(alphabet[x]);
+		x--;
+	}
+	return (0);
+}
