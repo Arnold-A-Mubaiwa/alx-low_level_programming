f000796f3f25a13762cb5e77ce416c951348c628
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-10 07:07:16 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+/**
+ * main - main entry of a program
+ *
+ * Return: return 0 (zero)
+ */
+int main(void)
+{
+	int x = 0;
+	char alphabet[] = "0123456789abcdef";
+
+	while (x <= 16)
+	{
+		putchar(alphabet[x]);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
