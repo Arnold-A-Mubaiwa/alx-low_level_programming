8d1aee13394cbcca05ed9c6915d69e19ea6414c4
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-10 07:39:18 -0700

    print combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@ int main(void)
 	int x = 0;
 	char alphabet[] = "0123456789";
 
-	while (x <= 10)
+	while (x < 10)
 	{
 		putchar(alphabet[x]);
-		if (alphabet[x] != 9)
+		if (alphabet[x] != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 		x++;
 	}
-	putchar('$');
+	/* putchar('$');*/
 	putchar('\n');
 	return (0);
 }
