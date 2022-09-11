53098e3812de6e604e834e9de0f31ce7b19bf1e6
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-11 01:36:03 -0700

    alphanumerc numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,7 +9,8 @@ int main(void)
 {
 	int x = 48;
 	/* char alphabet[] = "abcdefghijklmnopqrstuvwxyz";*/
-	/* alphanumeric characters {a-z} {0-9} {special char}
+	/**
+	 * alphanumeric characters {a-z} {0-9} {special char}
 	 * they start from {a-z} 65-90 and {0-9} 48-57
 	 * {A-Z} 97-122
 	 */
