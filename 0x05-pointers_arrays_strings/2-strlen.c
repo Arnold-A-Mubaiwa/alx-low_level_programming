2f2587db56bd3126b178af8f31308b689bd066ff
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2022-09-19 14:21:11 -0700

    The legth of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <string.h>
+/**
+ * _strlen - calculate the length of a string
+ * @s: is the string value
+ *
+ * Return: rerturn the length
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
